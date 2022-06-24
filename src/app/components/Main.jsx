f9493cf8d955aaaa44544c3302c7dbf56105a409
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:19:25 -0500

    navigation component

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -4,13 +4,17 @@ import {store} from '../store';
 import {ConnectedDashboard} from './Dashboard';
 import {Router, Route} from 'react-router-dom';
 import {history} from '../store/history';
+import {ConnectedNavigation} from './Navigation';
 
 export const Main = () => (
 	<Router history={history}>
 		<Provider store={store}>
-			<Route exact path='/dashboard' render={() => (
-				<ConnectedDashboard/>
-			)}/>
+			<div>
+				<ConnectedNavigation/>
+				<Route exact path='/dashboard' render={() => (
+					<ConnectedDashboard/>
+				)}/>
+			</div>
 		</Provider>
 	</Router>
 );
